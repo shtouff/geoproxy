ea7ec4874260b22d9cc3d29dc0e380bda45f43d2
Remi Paulmier <remi.paulmier@comuto.com>
2011-08-04 11:35:17 +0200

    config dir removal: shouldnot be in git

--- config/backend.redis.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'backend.servers.inc.php');
-
-// redis geoproxy server
-define("CFG_GEOPROXY_REDIS_MASTER", $backend_servers['localhost']);
-
-?>
